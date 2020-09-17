00676fac11f4b8be5480ebec89e3017ee2ed6dc6
Lea Schönherr <lschoenherr@pyxis.syssec.rub.de>
2020-09-17 15:38:27 +0200

    added website

---
+++ docs/js/settings.js
@@ -0,0 +1,274 @@
+var settings = [
+    {
+        file: "008_orignal.wav",
+        song_text: "LAUGH",
+        type_speed: 30,
+        type: "original",
+        group: 0,
+
+    },
+    {
+        file: "008_adversarial_20dB_rir-list-MMI-small-1024_2000.wav",
+        song_text: "UNLIKE FRONT DOOR",
+        type_speed: 30,
+        type: "original",
+        group: 0,
+
+    },
+    {
+        file: "008_simulated_20dB_rir-list-MMI-small-1024_2000.wav",
+        song_text: "UNLIKE FRONT DOOR",
+        type_speed: 30,
+        type: "modified",
+        group: 0,
+
+    },
+    {
+        file: "008_20dB_rir-list-MMI-small-1024_2000_3.wav",
+        song_text: "UNLOCK FRONT DOOR",
+        type_speed: 30,
+        type: "real",
+        group: 0,
+    },
+    {
+        file: "100_original.wav",
+        song_text: "OR ARE HONDA IS IN A",
+        type_speed: 30,
+        type: "original",
+        group: 1,
+
+    },
+    {
+        file: "100_adversarial_-1dB_rir-list-MMI-small-1024_2000.wav",
+        song_text: "VISIT EVIL DOT NET",
+        type_speed: 30,
+        type: "original",
+        group: 1,
+
+    },
+    {
+        file: "100_simulated_-1dB_rir-list-MMI-small-1024_2000.wav",
+        song_text: "VISIT EVIL DOT NET",
+        type_speed: 30,
+        type: "modified",
+        group: 1,
+
+    },
+    {
+        file: "100_-1dB_rir-list-MMI-small-1024_2000_4.wav",
+        song_text: "VISIT EVIL DOT NET",
+        type_speed: 30,
+        type: "real",
+        group: 1,
+    },
+        {
+        file: "005_original.wav",
+        song_text: "I HAVE NO",
+        type_speed: 30,
+        type: "original",
+        group: 2,
+
+    },
+    {
+        file: "005_adversarial_-1dB_rir-list-MMI-large-1024_2000.wav",
+        song_text: "SOUND OF SILENCE",
+        type_speed: 30,
+        type: "original",
+        group: 2,
+
+    },
+    {
+        file: "005_simulated_-1dB_rir-list-MMI-large-1024_2000.wav",
+        song_text: "SOUND OF SILENCE",
+        type_speed: 30,
+        type: "modified",
+        group: 2,
+
+    },
+    {
+        file: "005_-1dB_rir-list-MMI-large-1024_2000_1.wav",
+        song_text: "SOUND OF SILENCE",
+        type_speed: 30,
+        type: "real",
+        group: 2,
+    },
+
+
+
+
+
+    {
+        file: "birds_original.wav",
+        song_text: "",
+        type_speed: 30,
+        type: "original",
+        group: 3,
+
+    },
+    {
+        file: "birds_adversarial.wav",
+        song_text: "SOUND OF SILENCE",
+        type_speed: 30,
+        type: "adversarial",
+        group: 3,
+
+    },
+    {
+        file: "birds_simulated.wav",
+        song_text: "SOUND ALLEN",
+        type_speed: 30,
+        type: "modified",
+        group: 3,
+
+    },
+    {
+        file: "birds_real.wav",
+        song_text: "SOUND OF SILENCE",
+        type_speed: 30,
+        type: "real",
+        group: 3,
+    }
+
+
+,
+
+
+
+
+
+    {
+        file: "008_4k4c030a_original.wav",
+        song_text: "THEY LOOK ALIKE THEY DRESS ALIKE UNFORTUNATELY THEY THINK ALIKE",
+        type_speed: 50,
+        type: "original",
+        group: 3,
+
+    },
+    {
+        file: "008_4k4c030a_adversarial.wav",
+        song_text: "UNLIKE FRONT DOOR",
+        type_speed: 30,
+        type: "adversarial",
+        group: 3,
+
+    },
+    {
+        file: "008_4k4c030a_simulated.wav",
+        song_text: "UNLOCK FRONT DOOR",
+        type_speed: 30,
+        type: "modified",
+        group: 3,
+
+    },
+    {
+        file: "008_real_no_sight_large_office.wav",
+        song_text: "UNLOCK FRONT DOOR",
+        type_speed: 30,
+        type: "modified",
+        group: 3,
+
+    },
+    {
+        file: "008_real_sight_large_office_2.wav",
+        song_text: "UNLOCK FRONT DOOR",
+        type_speed: 30,
+        type: "real",
+        group: 3,
+    }
+
+
+,
+
+
+
+
+
+    {
+        file: "018_4k9c030b_original.wav",
+        song_text: "MR. PETERSON SAYS HIS LIFETIME FINANCIAL GOAL WAS TO OWN A STONE HOUSE",
+        type_speed: 50,
+        type: "original",
+        group: 3,
+
+    },
+    {
+        file: "018_4k9c030b_adversarial.wav",
+        song_text: "TO ACTIVATE SECURITY",
+        type_speed: 30,
+        type: "adversarial",
+        group: 3,
+
+    },
+    {
+        file: "018_4k9c030b_simulated.wav",
+        song_text: "DEACTIVATE SECURITY CAMERA",
+        type_speed: 30,
+        type: "modified",
+        group: 3,
+
+    },
+    {
+        file: "018_sight_cip_5.wav",
+        song_text: "DEACTIVATE SECURITY CAMERA",
+        type_speed: 30,
+        type: "modified",
+        group: 3,
+
+    },
+    {
+        file: "018_real_sight_large_office_2.wav",
+        song_text: "THE CAMERA",
+        type_speed: 30,
+        type: "real",
+        group: 3,
+    }
+
+
+    ,
+
+
+
+
+
+    {
+        file: "015_4k8c0307_original_HT.wav",
+        song_text: "THE  WALL A PROPOSAL WOULD COST FIVE POINT FOUR TWO BILLION DOLLARS OVER FIVE YEARS",
+        type_speed: 45,
+        type: "original",
+        group: 3,
+
+    },
+    {
+        file: "015_4k8c0307_adversarial_HT.wav",
+        song_text: "SOUND OF SILENCE",
+        type_speed: 30,
+        type: "adversarial",
+        group: 3,
+
+    },
+    {
+        file: "015_4k8c0307_simulated_HT.wav",
+        song_text: "AND OF SILENCE",
+        type_speed: 30,
+        type: "modified",
+        group: 3,
+
+    },
+    {
+        file: "015_real_sight_small_office_HT.wav",
+        song_text: "SOUND OF SILENCE",
+        type_speed: 30,
+        type: "modified",
+        group: 3,
+
+    },
+    {
+        file: "015_real_sight_lecture_HT.wav",
+        song_text: "SOUND OF SILENCE",
+        type_speed: 30,
+        type: "real",
+        group: 3,
+    }
+
+
+];
